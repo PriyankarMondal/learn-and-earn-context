4e944f8b25837ed4b9cfa1f940972844528289fb
PriyankarMondal <mpriyankar04@gmail.com>
2026-03-25 17:46:12 +0530

    Delete register directory

--- register/src/components/pages/SignUpPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SignUpContainer from "../container/SignUpContainer";
-
-
-const SignUpPage = () => {
-  return <SignUpContainer/>
-};
-
-export default SignUpPage;
